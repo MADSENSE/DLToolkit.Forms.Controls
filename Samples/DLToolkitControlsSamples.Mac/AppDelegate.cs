8e9a2b18c2711be3fae209bda6f5f36e4764cef7
Yann Zahringer Ferrando <yann.zahringer@madsense.com>
2018-01-22 17:10:33 +0100

    Sample Mac app added

---
+++ Samples/DLToolkitControlsSamples.Mac/AppDelegate.cs
@@ -0,0 +1,36 @@
+﻿using AppKit;
+using Foundation;
+using Xamarin.Forms.Platform.MacOS;
+using CoreGraphics;
+using Xamarin.Forms;
+
+namespace DLToolkitControlsSamples.Mac
+{
+    [Register("AppDelegate")]
+    public sealed class AppDelegate : FormsApplicationDelegate
+    {
+        public AppDelegate()
+        {
+            const NSWindowStyle style = NSWindowStyle.Titled | NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Miniaturizable;
+
+            var rect = new CGRect(200, 200, 1024, 768);
+            MainWindow = new NSWindow(rect, style, NSBackingStore.Buffered, false)
+            {
+                Title = "DLToolkitControlsSamples",
+                TitleVisibility = NSWindowTitleVisibility.Hidden
+            };
+            MainWindow.Center();
+
+            NSApplication.SharedApplication.Menu = new NSMenu();
+        }
+
+        public override NSWindow MainWindow { get; }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
+        }
+    }
+}
