9501ade7408eda2f7b056b8414ebc57d3a405d7b
Yann Zahringer Ferrando <yann.zahringer@madsense.com>
2018-01-22 17:40:32 +0100

    Resources & Renderers added on Sample app

---
+++ Samples/DLToolkitControlsSamples.Mac/Renderers/FlowListViewCellRenderer.cs
@@ -0,0 +1,18 @@
+﻿[assembly: Xamarin.Forms.ExportRenderer(typeof(DLToolkit.Forms.Controls.FlowListViewInternalCell), typeof(DLToolkitControlsSamples.Mac.Renderers.FlowListViewCellRenderer))]
+namespace DLToolkitControlsSamples.Mac.Renderers
+{
+    using AppKit;
+
+    using Xamarin.Forms;
+
+    public class FlowListViewCellRenderer : Xamarin.Forms.Platform.MacOS.ViewCellRenderer
+    {
+        public override NSView GetCell(Cell item, NSView reusableView, NSTableView tv)
+        {
+            var cell = base.GetCell(item, reusableView, tv);
+            // TODO [macOS] Implement: cell.SelectionStyle = AppKit.UITableViewCellSelectionStyle.None;
+
+            return cell;
+        }
+    }
+}
---
+++ Samples/DLToolkitControlsSamples.UWP/Renderers/FlowListViewCellRenderer.cs
@@ -0,0 +1,19 @@
+﻿[assembly: Xamarin.Forms.Platform.UWP.ExportRenderer(typeof(DLToolkit.Forms.Controls.FlowListView), typeof(DLToolkitControlsSamples.UWP.Renderers.FlowListViewCellRenderer))]
+namespace DLToolkitControlsSamples.UWP.Renderers
+{
+    using Xamarin.Forms.Platform.UWP;
+
+    public class FlowListViewCellRenderer : ListViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.ListView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (List != null)
+            {
+                List.SelectionMode = Windows.UI.Xaml.Controls.ListViewSelectionMode.None;
+                List.IsItemClickEnabled = false;
+            }
+        }
+    }
+}
